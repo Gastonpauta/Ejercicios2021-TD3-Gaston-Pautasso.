f98daac2f0165bc47ecdcb7c06a547c655d37d3c
gastonpauta <gaston_pauta@hotmail.com>
2021-03-29 18:13:37 -0300

    PRUEBA

--- Ejercicio_1_Secuencia_Led/src/main.c
+++ Ejercicio_1_Secuencia_Led/src/main.c
@@ -11,17 +11,7 @@ void app_main()
     gpio_pad_select_gpio(BLINK_GPIO);
 
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-
-    while (1)
-    {
-        /* code */
-        printf("LED ON");
-        gpio_set_level(BLINK_GPIO, 1); 
-        vTaskDelay(1000/portTICK_PERIOD_MS);
-
-        printf("LED OFF");
-        gpio_set_level(BLINK_GPIO, 0);
-        vTaskDelay(1000/portTICK_PERIOD_MS);        
+      
     }
     
 }
