f0dded6219d76dd38fc44a6471d583bd14e39f44
gastonpauta <gaston_pauta@hotmail.com>
2021-03-29 17:51:39 -0300

    EJEMPLO_1_SECUENCIA_LED

--- Ejercicio_1_Secuencia_Led/src/main.c
+++ Ejercicio_1_Secuencia_Led/src/main.c
@@ -1,1 +1,27 @@
-void app_main() {}
+void app_main() {
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+
+#define BLINK_GPIO CONFIG_BLINK_GPIO
+
+void app_main() 
+{
+    gpio_pad_select_gpio(BLINK_GPIO);
+
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (1)
+    {
+        /* code */
+        printf("LED ON");
+        gpio_set_level(BLINK_GPIO, 1); 
+        vTaskDelay(1000/portTICK_PERIOD_MS);
+
+        printf("LED OFF");
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(1000/portTICK_PERIOD_MS);        
+    }
+    
+}
